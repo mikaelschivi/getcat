2c20672b6f891f7f34cdba29ef9ef2070ba02c70
notebook <mikael>
2022-06-15 02:57:43 -0300

    .

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python3
 from asyncio import exceptions
-from logging import raiseExceptions
 from urllib.error import HTTPError
 
+import os
 import threading
 import requests
 
+path = '../getcat/img/'
 url = "https://cataas.com/cat"
 
-def downloadPic(name):
-    print('started downloading function')
+def createFolder(path):
+        try:
+            if os.path.isdir(path):
+                pass                
+            else:
+                os.makedirs(path)
+                print('created directory for storing.')
+        except IOError as exception:
+            raise IOError('%s: %s' % (path, exception.strerror))
+        return None
+
+
+def downloadCat(name):
+    print(f'[{name}] started download function')
     r = requests.get(url)
     hdr = r.headers['content-type']
 
     if hdr == 'image/jpeg':
-        open(f'pic{name}.jpeg', 'wb').write(r.content)
-        print(f'downloading pic{name}')
+        open(f'{path}cat{name}.jpeg', 'wb').write(r.content)
+        print(f'downloading cat{name}')
     elif hdr == 'image/png':
-        open(f'pic{name}.png', 'wb').write(r.content)
-        print(f'downloading pic{name}')
+        open(f'{path}cat{name}.png', 'wb').write(r.content)
+        print(f'downloading cat{name}')
     else:
         print('\n NEW TYPE FOUND',type(hdr),'\n')
         
 if __name__ == '__main__':
-    num = int(input('number of downloads:'))
+    createFolder(path)
+    num = int(input('number of downloads: '))
 
     try:
         for n in range(num):    
-            print(n)    
-            thread = threading.Thread(group=None,target=downloadPic,args=(n,))
+            thread = threading.Thread(group=None,target=downloadCat,args=(n,))
             thread.start()
             thread.join()
 
